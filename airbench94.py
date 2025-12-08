e62950b7ba899a31227c250fa421092088701e90
Tanay Tandon <tanaytandon7@gmail.com>
2025-12-08 02:59:14 -0500

    reset to base

--- airbench94.py
+++ airbench94.py
@@ -412,7 +412,7 @@ def infer(model, loader, tta_level=0):
         pad = 1
         padded_inputs = F.pad(inputs, (pad,)*4, 'reflect')
         inputs_translate_list = [
-            # padded_inputs[:, :, 0:32, 0:32],
+            padded_inputs[:, :, 0:32, 0:32],
             padded_inputs[:, :, 2:34, 2:34],
             # padded_inputs[:, :, 1:33, 1:33],
         ]
